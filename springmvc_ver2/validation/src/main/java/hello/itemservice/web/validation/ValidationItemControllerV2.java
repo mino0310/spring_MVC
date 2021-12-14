937e2225641d893f5df66219809c45bac63da35c
mino0310 <bewriter310@gmail.com>
2021-12-14 11:58:28 +0900

    Validator 분리2

--- springmvc_ver2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ springmvc_ver2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -10,6 +10,8 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -31,6 +33,11 @@ public class ValidationItemControllerV2 {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "validation/v2/items";
+    }
+
+    @InitBinder
+    public void init(WebDataBinder dataBinder) {
+        dataBinder.addValidators(itemValidator);
     }
 
     @GetMapping("/{itemId}")
@@ -197,10 +204,26 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
         itemValidator.validate(item, bindingResult);
+
+        // 여기까지 진행했는데 error 안에 값이 있으면 에러가 발생했다는 뜻임. 다시 입력 폼으로 돌려보내야 함
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {} ", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        // 성공 로직 (검증에 성공했다)
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
         // 여기까지 진행했는데 error 안에 값이 있으면 에러가 발생했다는 뜻임. 다시 입력 폼으로 돌려보내야 함
         if (bindingResult.hasErrors()) {
