2636d13106209085fcc61f0de608fe60dc5a9b4c
mino0310 <bewriter310@gmail.com>
2021-12-20 16:12:01 +0900

    Formatter 적용

--- springmvc_ver2/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
+++ springmvc_ver2/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.IpPortToStringConverter;
 import hello.typeconverter.converter.StringToIntegerConverter;
 import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,9 +13,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIpPortConverter());
+
+        // 우선순위 때문에 이 두개를 주석처리해주어야 함.
+//        registry.addConverter(new IntegerToStringConverter());
+//        registry.addConverter(new StringToIntegerConverter());
         registry.addConverter(new IpPortToStringConverter());
-        registry.addConverter(new IntegerToStringConverter());
-        registry.addConverter(new StringToIntegerConverter());
+        registry.addConverter(new StringToIpPortConverter());
+
+        // 추가
+        registry.addFormatter(new MyNumberFormatter());
+
     }
 }
